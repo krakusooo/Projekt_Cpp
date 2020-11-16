c9a4151d92f239c297c0b9311de77035b35d889b
krakusooo <62896332+krakusooo@users.noreply.github.com>
2020-11-16 12:48:11 +0100

    Update Baza_asortymentu.h

--- Baza_asortymentu.h
+++ Baza_asortymentu.h
@@ -22,44 +22,14 @@ class Assortment {
         friend class DatabaseAssortment;
         friend class coperator;
         
-    public:
-        void setFilmTitle (string film_title) {
-            this -> film_title = film_title;
-        }
-        void setFilmType(string film_type) {
-            this -> film_type = film_type;
-        }
-        void setProductionYear (int production_year) {
-            this -> production_year = production_year;
-        }
-        void setDirector (string director) {
-            this -> director = director;
-        }
-        void setPricePerDay (float price_per_day) {
-            this -> price_per_day = price_per_day;
-        }
-        void setPricePerWeek (float price_per_week) {
-            this -> price_per_week = price_per_week;
-        }
+        int getId()const{return id;}
+        string getFilmTitle()const{return film_title;}
+        string getFilmType()const{return film_type;}
+        int getProductionName()const{return production_year;}
+        string getDirector()const{return director;}
+        float getPricePerDay()const{return price_per_day;}
+        float getPricePerWeek()const{return price_per_week;}
 
-        string getFilmTitle ()const {
-            return film_title;
-        }
-        string getFilmType ()const {
-            return film_type;
-        }
-        int getProductionYear ()const {
-            return production_year;
-        }
-        string getDirector ()const {
-            return director;
-        }
-        float getPricePerDay ()const {
-            return price_per_day;
-        }
-        float getPricePerWeek ()const {
-            return price_per_week;
-        }
 };
 
 struct comperator {
@@ -68,24 +38,34 @@ struct comperator {
     }
 };
 
-class DatabaseAssortment {
-    fstream databaseassortment;
-    set<Assortment, comperator>Database;
-    set<Assortment, comperator>::iterator it;
+class DatabasAssorment {
+    fstream database_assortment;
 
-    public:
-        set<Assortment, comperator>::iterator node;
-        void findId (int data);
-        void findFilmTitle (string data);
-        void findFilmtype (string data);
-        void findProductionYear (int data);
-        void findDirector (string data);
-        void findPricePerDay (float data);
-        void findPricePerWeek (float data);
-        void insertNode (int id, string film_title, string film_type, int production_year, string director, float price_per_day, float price_per_week);
-        void eraseNode();
-        void saveInFile();
-        void loadFromFile();
+    set<Assortment, comperator> Database;
+    swt<Assortment, comperator> ::iterator it;
+
+public:
+    int last_id;
+    set<Assortment, comperator> ::iterator node;
+    DatabaseAssortment(int id = 0){this -> last_id = id;}
+
+    bool findId(int data);
+    bool findFilmTitle(string data);
+    bool findFilmType(string data);
+    bool findProductionYear(int data);
+    bool findDirector(string data);
+    bool findPricePerDay(float data);
+    bool findPricePerWeek(float data);
+
+    void insertNode(int id, string film_title, string film_type, int production_year, string director, float price_per_day, float price_per_week);
+    void eraseNode();
+    void saveInFile();
+    bool loadFromFile();
+
+    void setFilmTitle(string data);
+    void setFilmtype(string data);
+    void setProductionYear(int data);
+    void setDirector(string data);
+    void setPricePerDay(float data);
+    void setPricePerWeek(float data);
 };
-
-#endif
